0c4c8a6ac827b6d2721c8cca845d40dc6cf3710d
delee5695 <delee5695@naver.com>
2022-08-01 01:49:57 +0900

    Student - trinity circle

--- src/pages/Club.js
+++ src/pages/Club.js
@@ -8,23 +8,7 @@ const Club = () => {
           <img src={schoolBackground}></img>
           <h2>Club</h2>
         </div>
-        <div className='trinity'>
-          <h3>Student Coucil</h3>
-          <div className='circle'></div>
-          <div className='student' id='council'>
-            <img src={studentCouncil}/>
-            <h6>Student Council</h6>
-          </div>
-          <div className='student' id='court'>
-            <img src={studentCouncil}/>
-            <h6>Student Court</h6>
-          </div>
-          <div className='student' id='congress'>
-            <img src={studentCouncil}/>
-            <h6>Student Congress</h6>
-          </div>
-          
-        </div>
+      
       </body>
     );
   };
