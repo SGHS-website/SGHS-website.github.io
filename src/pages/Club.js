70efc3adeaece7c543da0f3fb9dfd5b8f71ff935
유승현 <sophia1601@naver.com>
2022-08-01 23:34:15 +0900

    승현

--- src/pages/Club.js
+++ src/pages/Club.js
@@ -1,6 +1,29 @@
 import React from 'react';
+import "../../src/styles/Club.css";
 const Club = () => {
-    return <h1>Club</h1>;
+    return (
+    <><h1>Club</h1><div className="Club">
+        <div className="banner"></div>
+        <div className="content1">
+          <h2>ACADEMICS</h2>
+
+        </div>
+        <div className="banner"></div>
+        <div className="content2">
+          <h2>ARTS</h2>
+        </div>
+        <div className="banner"></div>
+        <div className="content3">
+          <h2>SPORTS</h2>
+        </div>
+        <div className="banner"></div>
+        <div className="content4">
+          <h2>VOLUNTEERING</h2>
+        </div>
+      </div></>
+    );
+
+
   };
   
 export default Club;
