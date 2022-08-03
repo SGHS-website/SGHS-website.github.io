95fd5d5dab2d04090113c3a17f308538be941bf3
danathtt <taehuig40@gmail.com>
2022-08-03 22:52:08 +0900

    수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,24 +8,47 @@ import { Parallax, ParallaxLayer } from "@react-spring/parallax"
 const Home = () => {
     return (
         <div>
-            <Parallax pages={6} style={{ backgroundColor: "black" }}>
+            <Parallax pages={4} style={{ backgroundColor: "black" }}>
                 <ParallaxLayer id="one">
                     <img src={schoolBackground} className="backgroundImage" />
                 </ParallaxLayer>
-                <ParallaxLayer id="two" offset={1} speed={0.5} factor={1}>
-                    <span className="smallletters">certified by </span><span className="bigletters">UNESCO</span><img src={Ulogo} className="logoImage"/>
+                <ParallaxLayer id="two" offset={1} speed={2}>
+                    <script>
+                    function reveal() { function reveal() {
+                        var reveals = document.querySelectorAll(".reveal");
+                        for (var i = 0; i < reveals.length; i++) {
+                            var windowHeight = window.innerHeight;
+                            var elementTop = reveals[i].getBoundingClientRect().top;
+                            var elementVisible = 150;
+                            if (elementTop < windowHeight - elementVisible) {
+                            reveals[i].classList.add("active");
+                            } else {
+                            reveals[i].classList.remove("active");
+                            }
+                        }
+                      }
+                    }
+                    window.addEventListener("scroll", reveal);
+                    </script>
+                    <div className="reveal">
+                        <span className="smallletters">certified by </span><span className="bigletters">UNESCO</span><img src={Ulogo} className="logoImage"/>
+                    </div>
+                    <div className="reveal">
+                        <span className="bigletters">INTELLIGENCE</span>
+                    </div>
+                    <div className="reveal">
+                        <span className="bigletters">VIRTUE</span>
+                    </div>
+                    <div className="reveal">
+                        <span className="bigletters">STRENGTH</span>
+                    </div>
+                    <div className="reveal">
+                        <span className="smallletters">certified by </span><span className="bigletters">WASC</span><img src={Wlogo} className="logoImage" />
+                    </div>
                 </ParallaxLayer>
-                <ParallaxLayer id="three" offset={2} speed={0.5} factor={1}>
-                    <span className="bigletters">INTELLIGENCE</span>
+                <ParallaxLayer id="three" offset={2} speed={2} style={{ display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "red"}}>
                 </ParallaxLayer>
-                <ParallaxLayer id="four" offset={3} speed={0.5} factor={1}>
-                    <span className="bigletters">VIRTUE</span>
-                </ParallaxLayer>
-                <ParallaxLayer id="five" offset={4} speed={0.5} factor={1}>
-                    <span className="bigletters">STRENGTH</span>
-                </ParallaxLayer>
-                <ParallaxLayer id="six" offset={5} speed={0.5} factor={1}>
-                    <span className="smallletters">certified by </span><span className="bigletters">WASC</span><img src={Wlogo} className="logoImage" />
+                <ParallaxLayer id="four" offset={3} speed={2} style={{ display: "flex", justifyContent: "center", alignItems: "center"}}>
                 </ParallaxLayer>
             </Parallax>
         </div>
