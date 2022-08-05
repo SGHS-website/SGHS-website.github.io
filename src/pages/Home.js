f6fa87377028ef43112f5da7420d8c4c9d2517dc
danathtt <taehuig40@gmail.com>
2022-08-05 15:21:49 +0900

    이안 선배 버전 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import schoolBackground from "../images/background.jpg"
 import keywordVideo from "../images/video.mp4"
+import keywordVideo2 from "../images/video2.mp4"
 import schoolLogo from "../images/logo.png"
 import '../styles/App.css'
 import { Parallax, ParallaxLayer } from "@react-spring/parallax"
@@ -15,6 +16,7 @@ const Home = () => {
                     <video src={keywordVideo} autoplay="autoplay" muted loop="true" width="80%" playsInline></video>
                 </ParallaxLayer>
                 <ParallaxLayer id="three" offset={2} speed={0.5}>
+                    <video src={keywordVideo2} autoplay="autoplay" muted loop="true" width="80%" playsInline></video>
                 </ParallaxLayer>
                 <ParallaxLayer id="four" offset={3} speed={0.5}>
                 </ParallaxLayer>
