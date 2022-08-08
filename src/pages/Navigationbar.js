7764f42f5a3c61598c4cebd2efb7582ab309b639
유승현 <sophia1601@naver.com>
2022-08-09 00:04:11 +0900

    승현

--- src/pages/Navigationbar.js
+++ src/pages/Navigationbar.js
@@ -1,19 +1,19 @@
 import { Nav, Navbar, NavLink } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import React from 'react';
-
 const Navigationbar = () => {
     return (
         <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
             <Navbar.Toggle aria-controls="navbarScroll" data-bs-toggle="collapse" data-bs-target="#navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
-                <Nav>
-                    <NavLink  eventKey="1" as={Link} to="/">Home</NavLink>
-                    <NavLink  eventKey="2" as={Link} to="/club">Club</NavLink>
-                    <NavLink  eventKey="3" as={Link} to="/event">Event</NavLink>
-                    <NavLink  eventKey="4" as={Link} to="/competition">Competition</NavLink>
-                    <NavLink  eventKey="5" as={Link} to="/student">Stuent</NavLink>
-                    <NavLink  eventKey="6" as={Link} to="/faq">Faq</NavLink>
+                <Nav className="justify-content-end" style={{ width: "100%", fontSize: "20px", marginBottom: "2px", marginTop:"-2px"}}>
+                <p style={{width: "10", fontSize: "12px", textAlign: "left", marginTop: "6px", marginBottom: "-6px", marginRight:"50%"}}>Seoul Global <br></br>High School</p>
+                    <NavLink  eventKey="1" as={Link} to="/" style={{marginRight:"2%"}}>Home</NavLink>
+                    <NavLink  eventKey="2" as={Link} to="/club" style={{marginRight:"2%"}}>Club</NavLink>
+                    <NavLink  eventKey="3" as={Link} to="/event" style={{marginRight:"2%"}}>Event</NavLink>
+                    <NavLink  eventKey="4" as={Link} to="/competition" style={{marginRight:"2%"}}>Competition</NavLink>
+                    <NavLink  eventKey="5" as={Link} to="/student" style={{marginRight:"2%"}}>Stuent</NavLink>
+                    <NavLink  eventKey="6" as={Link} to="/faq" style={{marginRight:"2%"}}>Faq</NavLink>
                 </Nav>
             </Navbar.Collapse>     
         </Navbar>
