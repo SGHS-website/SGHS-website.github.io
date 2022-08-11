f72a9b2dd38946b8958119a1c408a6e0b87648f3
lee56781 <221612@sghs.sen.hs.kr>
2022-08-11 19:39:15 +0900

    add html file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,26 @@ const Home = () => {
                     <h2>section3</h2>
                 </ParallaxLayer>
                 <ParallaxLayer id="four" offset={3} speed={0.5}>
-                    
+                    <div className="container">
+                        <div className="one">
+                            <h1>WE THE STUDENTS, DISCOVER OUR OWN TALENTS</h1>
+                            <h2>
+                              <a href="club.html">-club</a>
+                            </h2>
+                        </div>
+                        <div className="two">
+                            <h1>WE THE STUDENTS, INSPIRE THE WORLD BY OUR SELF-EMPOWERMENT</h1>
+                            <h3>
+                              <a href="school events.html">-school events</a>
+                            </h3>
+                        </div>
+                        <div className="three">
+                            <h1>WE THE STUDENTS, DEVELOP THIS WEBSITE</h1>
+                            <h4>
+                              <a href="website.html">-website</a>
+                            </h4>
+                        </div>
+                    </div>
                 </ParallaxLayer>
             </Parallax>
         </div>
