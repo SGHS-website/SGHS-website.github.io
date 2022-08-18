a00747aaea2c9fe51c318c8de819ffca24258281
syslisa <syslisa0609@gmail.com>
2022-08-18 18:22:48 +0900

    footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,6 +20,22 @@ const Home = () => {
                     
                 </ParallaxLayer>
             </Parallax>
+            
+            <div className="Footer">
+               <div className="first">
+                   <p>[03066]서울 종로구 성균관로13길 40 <br></br> (명륜1가, 서울국제고등학교)</p>
+                   <p> Sponsored by 종로구청 <br></br> Developed by 설국다움</p>
+                   <p>@Copyright</p>
+               </div>
+               <div className="last">
+                   <p>Contact Us</p>
+                   <img alt="envelope.png" className="envelope"></img>
+                   <img alt="insta.png" className="insta"></img>
+                   <img alt="image 13.png" className="image13"></img>
+                   <img alt="설국다움.png" className="설국다움"></img>
+                </div>
+            </div>
+        
         </div>
     )
 };
