927f210171508d3faff90d5d91c63d48df11b6f6
유승현 <sophia1601@naver.com>
2022-08-23 18:54:26 +0900

    .

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,6 +1,7 @@
 import React from "react";
 import schoolBackground from "../images/background.jpg"
 import "../../src/styles/Faq.css";
+
 const Faq = () => {
     return(
       <body>
@@ -10,13 +11,15 @@ const Faq = () => {
         </div>
         <div className="question">
           <p className="ask">Have a Question?</p>
-          <p classname="inf">설문지/오픈채팅방을 통해 질문을 받고 있습니다! <br></br>재학생용과 외부인용으로 나누어져 있으니 구분하여<br></br>작성해주시길 바랍니다.
-          <br></br>대표 질문은 교육부에서 선발 후 FAQs로 게시합니다.<br></br><br></br>질문링크</p>
-          <div className="link">
-          <p>재학생용</p>
-          <p>외부인용</p>
-          </div>
+          <p className="inf-faq">설문지/오픈채팅방을 통해 질문을 받고 있습니다!<br></br>재학생용과 외부인용
+          <span classname="inf-faq-1">(입학 예정자, 입학 희망자)</span>으로 나누어져 있으니 구분하여<br></br>작성해주시기 바랍니다.
+            <br></br>대표 질문은 교육부에서 선발 후 FAQs로 게시합니다.<br></br><br></br><br></br>질문링크</p>
+
+          <p className="link-in">재학생용</p>
+          <p className="link-out">외부인용</p>
+          <p className="faq-line">   </p>
         </div>
+
 
       </body>
     );;
