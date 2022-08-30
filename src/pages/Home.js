ae71b87addfe86357beea12ddd956297c85985f1
Haeun25 <haeun32444238@gmail.com>
2022-08-30 19:15:15 +0900

    home error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,6 @@ const Home = () => {
                     <img src={schoolBackground} className="backgroundImage" />
                 </ParallaxLayer>
                 <ParallaxLayer id="two" offset={1} speed={0.5}>
-<<<<<<< HEAD
                     <video src={keywordVideo} autoplay="autoplay" muted loop="true" width="85%" playsInline></video>
                 </ParallaxLayer>
                 <ParallaxLayer id="three" offset={2} speed={0.5}>
@@ -20,15 +19,13 @@ const Home = () => {
                 </ParallaxLayer>
                 <ParallaxLayer id="four" offset={3} speed={0.5}>
                    <h2>Section4</h2>
-=======
-                    <h2>section2</h2>
+                <h2>section2</h2>
                 </ParallaxLayer>
                 <ParallaxLayer id="three" offset={2} speed={0.5}>
                     <h2>section3</h2>
                 </ParallaxLayer>
                 <ParallaxLayer id="four" offset={3} speed={0.5}>
                     <h2>section4</h2>
->>>>>>> 65d65b652c6d3de3c9bba3509f9356afe591a748
                 </ParallaxLayer>
             </Parallax>
             <div className="footer">
