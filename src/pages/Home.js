e333cf2364683abe82150b0a2872ebd778bbb41a
syslisa <syslisa0609@gmail.com>
2022-09-01 19:23:51 +0900

    recent news story

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,52 +19,60 @@ const Home = () => {
                     </div>
                     <div id="section3_content">
                         <div className="item" id="item1">
-                            <h2>Title</h2>
-                            <p>Heyyyyyyyyyyyyyyyy. This is recents news. blah blah blah blahblah blahblah blahvblah blahblah blahblah blah </p>
+                            <h2>설국다움</h2>
+                            <p></p>
                         </div>
                         <div className="item" id="item2">
-                            <h2>Title</h2>
-                            <p>Heyyyyyyyyyyyyyyyy. This is recents news. blah blah blah blahblah blahblah blahvblah blahblah blahblah blah </p>
+                            <h2>15기 타임 캡슐 저장식</h2>
+                            <p> </p>
                         </div>
                         <div className="item" id="item3">
-                            <h2>Title</h2>
-                            <p>Heyyyyyyyyyyyyyyyy. This is recents news. blah blah blah blahblah blahblah blahvblah blahblah blahblah blah </p>
+                            <h2>Sports Day</h2>
+                            <p>Sports day was held on July 14th 2022. For the opening ceromony each classes had a parade around the school pitch. 
+                               Then the class match finals of 1st and 2nd grades were held consequtively. 
+                               The class 2 of the first grade played handball with class 6, and the class 2 took the victory. 
+                               The sophomores played volleyball. class won against the class. 
+                               While the class matches were happening there were lots of booths ran by the school council. 
+                               After students had time to hang around the various booths, at 4pm, freshman and sophomore of each '누리' and '마루' team showed of their amazing cheerleading that they had practiced for weeks. 
+                               As soon as the cheerleading ended, the team relay was held. The result of the relay was the victory for the team 누리. 
+                               Between the sports day events, art clubs presented splendid performances. 
+                               The Sports day ended at 17:00 with the final speech form our old school council and a huge round of applause from the SGHS students.     </p>
                         </div>
                         <div className="item" id="item4">
                             <h2>Title</h2>
-                            <p>Heyyyyyyyyyyyyyyyy. This is recents news. blah blah blah blahblah blahblah blahvblah blahblah blahblah blah </p>
+                            <p>내용 </p>
                         </div>
                         <div className="item" id="item5">
                             <h2>Title</h2>
-                            <p>Heyyyyyyyyyyyyyyyy. This is recents news. blah blah blah blahblah blahblah blahvblah blahblah blahblah blah </p>
+                            <p>내용 </p>
                         </div>
                         <div className="item" id="item6">
                             <h2>Title</h2>
-                            <p>Heyyyyyyyyyyyyyyyy. This is recents news. blah blah blah blahblah blahblah blahvblah blahblah blahblah blah </p>
+                            <p>내용 </p>
                         </div>
                         <div className="item" id="item7">
                             <h2>Title</h2>
-                            <p>Heyyyyyyyyyyyyyyyy. This is recents news. blah blah blah blahblah blahblah blahvblah blahblah blahblah blah </p>
+                            <p>내용 </p>
                         </div>
                         <div className="item" id="item8">
                             <h2>Title</h2>
-                            <p>Heyyyyyyyyyyyyyyyy. This is recents news. blah blah blah blahblah blahblah blahvblah blahblah blahblah blah </p>
+                            <p>내용 </p>
                         </div>
                         <div className="item" id="item9">
                             <h2>Title</h2>
-                            <p>Heyyyyyyyyyyyyyyyy. This is recents news. blah blah blah blahblah blahblah blahvblah blahblah blahblah blah </p>
+                            <p>내용 </p>
                         </div>
                         <div className="item" id="item10">
                             <h2>Title</h2>
-                            <p>Heyyyyyyyyyyyyyyyy. This is recents news. blah blah blah blahblah blahblah blahvblah blahblah blahblah blah </p>
+                            <p>내용 </p>
                         </div>
                         <div className="item" id="item11">
                             <h2>Title</h2>
-                            <p>Heyyyyyyyyyyyyyyyy. This is recents news. blah blah blah blahblah blahblah blahvblah blahblah blahblah blah </p>
+                            <p>내용 </p>
                         </div>
                         <div className="item" id="item12">
                             <h2>Title</h2>
-                            <p>Heyyyyyyyyyyyyyyyy. This is recents news. blah blah blah blahblah blahblah blahvblah blahblah blahblah blah </p></div>                  
+                            <p>내용 </p></div>                  
                     </div>
                     
                 </ParallaxLayer>
