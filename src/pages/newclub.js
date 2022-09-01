0241feb8033e1ff921ab5d86a203230391912fb7
Haeun25 <haeun32444238@gmail.com>
2022-09-01 19:18:07 +0900

    club error fix

--- src/pages/newclub.js
+++ src/pages/newclub.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import schoolBackground from "../images/background.jpg";
-import studentCouncil from "../images/studentCouncil.png"
-import ".../src/styles/newclub.css";
+import "../styles/newclub.css";
 const Club = () => {
     return(
     <body>
@@ -11,16 +10,16 @@ const Club = () => {
       </div>
       <div className="Club1">
           <img src="https://cdn.pixabay.com/photo/2020/09/24/16/50/board-5599231_960_720.png" alt="img"></img>
-          <p className="academics"><a href= "#academics"> ACADEMICS </a></p>
+          <p className="academics"><a href= "#academic"> ACADEMICS </a></p>
           <img src="https://cdn.pixabay.com/photo/2020/09/24/16/50/board-5599231_960_720.png" alt="img"></img>
-          <p className="arts"><a href= "#arts"> ARTS </a></p>
+          <p className="arts"><a href= "#art"> ARTS </a></p>
           <img src="https://cdn.pixabay.com/photo/2020/09/24/16/50/board-5599231_960_720.png" alt="img"></img>
-          <p className="sports"><a href= "#sports"> SPORTS </a></p>
+          <p className="sports"><a href= "#sport"> SPORTS </a></p>
           <img src="https://cdn.pixabay.com/photo/2020/09/24/16/50/board-5599231_960_720.png" alt="img"></img>
-          <p className="volunteering"><a href= "#volunteering"> VOLUNTEERING</a></p>
+          <p className="volunteering"><a href= "#volunteerings"> VOLUNTEERING</a></p>
       </div>
 
-    <h1 class="category1"><div id="academics"> ACADEMICS </div></h1>
+    <h1 class="category1"><div id="academic"> ACADEMICS </div></h1>
         <div>
             <ul class="list1">
             <li><b><h2> SOCIETY </h2></b></li>
@@ -155,7 +154,7 @@ const Club = () => {
         </ul>
       </div>
 
-    <h1 class="category2"><div id="arts"> ARTS </div></h1>
+    <h1 class="category2"><div id="art"> ARTS </div></h1>
     <div>
         <ul class="list1">
             <div class="sub-category">
@@ -199,7 +198,7 @@ const Club = () => {
         </ul>
     </div>
 
-    <h1 class="category2"><div id="sports"> SPORTS </div></h1>
+    <h1 class="category2"><div id="sport"> SPORTS </div></h1>
     <div>
         <ul class="list1">
             <div class="sub-category">
@@ -247,7 +246,7 @@ const Club = () => {
         </ul>
     </div>
 
-    <h1 class="category2"><div id="volunteering"> VOLUNTEERING </div></h1>
+    <h1 class="category2"><div id="volunteerings"> VOLUNTEERING </div></h1>
     <div>
         <ul class="list1">
             <div class="sub-category">
