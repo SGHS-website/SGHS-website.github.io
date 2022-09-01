2bb462f219720ce3d0f44fc99888ef286c8b693b
유승현 <sophia1601@naver.com>
2022-09-01 18:41:31 +0900

    .

--- src/pages/Event.js
+++ src/pages/Event.js
@@ -6,6 +6,7 @@ import nurije from "../images/Nurije.png"
 import capable from "../images/Capable3.jpg"
 import vank from "../images/VANKMUN3.jpg"
 import "../../src/styles/Event.css";
+import { Link } from "react-router-dom";
 const Event = () => {
     return (
       <body>
@@ -16,25 +17,41 @@ const Event = () => {
         <div>
         <h1 className="Sc-Ev">School Events</h1>
         <div className="event">
-          <img src={jointconference} alt="img"></img>
-          <p className="e1">2021 연합학술제</p>
-          <img src={sportsfestival} alt="img"></img>
-          <p className="e2">2021 체육대회</p>
-          <img src={nurije} alt="img"></img>
-          <p className="e3">2021 누리제</p>
-          <img src={sportsfestival} alt="img"></img>
-          <p className="e4">VOLUNTEERING</p>
+          <Link to="/club">
+            <img src={jointconference} alt="img"></img>
+          < p className="e1">2021 연합학술제</p>
+          </Link>
+          <Link to="/club">
+           <img src={sportsfestival} alt="img"></img>
+            <p className="e2">2021 체육대회</p>
+          </Link>
+          <Link to="/club">
+            <img src={nurije} alt="img"></img>
+            <p className="e3">2021 누리제</p>
+          </Link>
+          <Link to="/club">
+            <img src={sportsfestival} alt="img"></img>
+            <p className="e4">VOLUNTEERING</p>
+          </Link>
         </div>
         <h1 className="Sc-Ev">Small-Scale Events</h1>
         <div className="event">
-          <img src={capable} alt="img"></img>
-          <p className="se1">Capable</p>
-          <img src={vank} alt="img"></img>
-          <p className="se2">VANK & MUNlit MUN</p>
-          <img src={nurije} alt="img"></img>
-          <p className="se3">2021 누리제</p>
-          <img src={sportsfestival} alt="img"></img>
-          <p className="se4">VOLUNTEERING</p>
+        <Link to="/club">
+            <img src={capable} alt="img"></img>
+            <p className="se1">Capable</p>
+          </Link>
+          <Link to="/club">
+            <img src={vank} alt="img"></img>
+            <p className="se2">VANK & MUNlit MUN</p>
+          </Link>
+          <Link to="/club">
+           <img src={nurije} alt="img"></img>
+            <p className="se3">2021 누리제</p>
+          </Link>
+          <Link to="/club">
+            <img src={sportsfestival} alt="img"></img>
+            <p className="se4">VOLUNTEERING</p>
+          </Link>
         </div>
         </div>
       </body>
