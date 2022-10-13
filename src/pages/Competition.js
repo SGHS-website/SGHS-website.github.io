851d97e92efa2aad22d7566d922978f114663371
delee5695 <delee5695@naver.com>
2022-10-13 19:19:03 +0900

    AboutUs

--- src/pages/Competition.js
+++ src/pages/Competition.js
@@ -11,69 +11,57 @@ const Competition = () => {
         
         {/*여기부터*/}
 
-        <h1 className="who">교장선생님</h1>
-        <div className="principal">
-            <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
-            <p><br></br><br></br><s>말씀</s> <br></br><br></br> sdafjoipasd sdfoia psdf sdaoi sdafoipasdf aosipdfas oidf soaidfajsopd dsa fjdsaifosdf oisdf aoids aiofsda oifs daiofsda ofisdfh oisad ofisd hoiafsda foisdhfio sda oifshoi dafsah iodfs doiafsd aio sdaiof fklsdanlk; sadfiopasd fsadflksadfhosdf sh sdafhiosd fsa diofhsd fasdoif asd oiabced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o sabced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o sabced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
-        </div>
-        <h1 className="who">학생회장</h1>
-        <div className="president">
-            <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
-            <p><br></br><br></br><s>말씀</s> <br></br><br></br> sdafjoipasd sdfoia psdf sdaoi sdafoipasdf aosipdfas oidf soaidfajsopd dsa fjdsaifosdf oisdf aoids aiofsda oifs daiofsda ofisdfh oisad ofisd hoiafsda foisdhfio sda oifshoi dafsah iodfs doiafsd aio sdaiof fklsdanlk; sadfiopasd fsadflksadfhosdf sh sdafhiosd fsa diofhsd fasdoif asd oiabced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o sabced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o sabced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
-        </div>
-        <h1 className="who">설국다움 크루</h1>
+        <h1 className="who">Our Team</h1>
         <div className="crew">
-        <div className="Us">
-            <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
-            <p className="member">소감 <br></br>abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
+            <div className="Us">
+                <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
+                <p className="member">Daeun Lee | 이다은<br></br>Lead Developer</p>    
+            </div>
+            <div className="Us">
+                <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
+                <p className="member">Rian Park | 박이안<br></br>Lead Designer</p>    
+            </div>
+            <div className="Us">
+            <img src={require("../images/aboutus/Irene.jpeg")}/>
+                <p className="member">Irene Huh | 허서윤 <br></br>Lead Treasurer</p>    
+            </div> 
+            <div className="Us">
+                <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
+                <p className="member">소감 <br></br>abced sd o s</p>    
+            </div> 
+            <div className="Us">
+                <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
+                <p className="member">소감 <br></br>abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
+            </div> 
+            <div className="Us">
+                <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
+                <p className="member">소감 <br></br>abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
+            </div> 
+            <div className="Us">
+                <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
+                <p className="member">소감 <br></br>abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
+            </div> 
+            <div className="Us">
+                <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
+                <p className="member">소감 <br></br>abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
+            </div> 
+            <div className="Us">
+                <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
+                <p className="member">소감 <br></br>abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
+            </div> 
+            <div className="Us">
+                <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
+                <p className="member">소감 <br></br>abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
+            </div> 
+            <div className="Us">
+                <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
+                <p className="member">소감 <br></br>abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
+            </div> 
+            <div className="Us">
+                <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
+                <p className="member">소감 <br></br>abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
+            </div>  
         </div>
-        <div className="Us">
-            <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
-            <p className="member">소감 <br></br>abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
-        </div>
-        <div className="Us">
-            <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
-            <p className="member">소감 <br></br>abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
-        </div> 
-        <div className="Us">
-            <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
-            <p className="member">소감 <br></br>abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
-        </div> 
-        <div className="Us">
-            <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
-            <p className="member">소감 <br></br>abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
-        </div> 
-        <div className="Us">
-            <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
-            <p className="member">소감 <br></br>abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
-        </div> 
-        <div className="Us">
-            <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
-            <p className="member">소감 <br></br>abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
-        </div> 
-        <div className="Us">
-            <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
-            <p className="member">소감 <br></br>abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
-        </div> 
-        <div className="Us">
-            <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
-            <p className="member">소감 <br></br>abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
-        </div> 
-        <div className="Us">
-            <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
-            <p className="member">소감 <br></br>abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
-        </div> 
-        <div className="Us">
-            <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
-            <p className="member">소감 <br></br>abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
-        </div> 
-        <div className="Us">
-            <img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMjdfODIg/MDAxNDkwNjEwNDA0MzM0.c4SZEA5JFpJcc40a-l2EqRVpjtg2hk57F0NJER3yXoEg.I4JHmmJgg7hxe-bs0CvJkm9FgClJ3am8y8NjTFa420Ug.JPEG.achika0123/muji.jpg?type=w800" alt="img"></img>
-            <p className="member">소감 <br></br>abced sdafjkl;cjfas cijovsdf sdjafiosdfasdhio sdaosd af sdaisd o s</p>    
-        </div>  
-        </div>
-
-                {/*여기까지*/}
 
       </body>
     );
