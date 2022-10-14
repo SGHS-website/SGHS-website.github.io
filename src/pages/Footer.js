a9ba3b8f520a2a20599d6e3bd5ac5b35d9ba9880
delee5695 <delee5695@naver.com>
2022-10-14 10:01:26 +0900

    Footer

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -6,9 +6,12 @@ const Footer = () =>{
         <div className="footer">
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
             <div className="first">
-                <p>[03066]서울 종로구 성균관로13길 40 <br></br> (명륜1가, 서울국제고등학교)</p>
-                <p> Sponsored by 종로구청 <br></br> Developed by 설국다움</p>
-                <p>@Copyright</p>
+                <p>[03066] 서울 종로구 성균관로13길 40 
+                    <br/> (명륜1가, 서울국제고등학교)
+                    <br/><br/>Sponsored by 종로구청
+                    <br/>Developed by 설국다움
+                    <br/><br/>@Copyright
+                </p>
             </div>
             <div className="last">
                 <p>Contact Us</p>
