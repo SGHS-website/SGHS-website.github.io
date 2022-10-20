108664e86b2c68959360965284d1554e9a9c018e
유승현 <sophia1601@naver.com>
2022-10-20 19:36:59 +0900

    Student Detail

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 import Home from './Home';
 import Club from './Club';
 import Event from './Event';
-import AboutUs from './AboutUs';
+import AboutUs from './Aboutus';
 import Student from './Student';
 import Faq from './Faq';
 import Eventde from './eventde'
 import ClubDetail from './clubDetail'
 import Navigationbar from './Navigationbar';
+import StudentCongress from './StudentCongress';
+import StudentCouncil from './StudentCouncil';
+import StudentCourt from './StudentCourt';
 function App() {
   return (
     <div className="App">
@@ -23,6 +26,9 @@ function App() {
                 <Route path='/faq' element={<Faq />}/>
                 <Route path = '/eventde' element={<Eventde/>}/>
                 <Route path = '/clubDetail' element={<ClubDetail/>}/>
+                <Route path = '/StudentCongress' element={<StudentCongress/>}/>
+                <Route path = '/StudentCouncil' element={<StudentCouncil/>}/>
+                <Route path = '/StudentCourt' element={<StudentCourt/>}/>
             </Routes>
         </Router>
         
--- src/pages/Student.js
+++ src/pages/Student.js
@@ -2,7 +2,7 @@ import React from "react";
 import schoolBackground from "../images/background.jpg"
 import studentCouncil from "../images/studentCouncil.png"
 import Footer from './Footer'
-
+import { Link } from "react-router-dom";
 const Student = () => {
     return (
       <body>
@@ -14,17 +14,22 @@ const Student = () => {
           <h3>Student Coucil</h3>
           <div className='circle'></div>
           <div className='student' id='council'>
-            <img src={studentCouncil}/>
-            <a href='#councilPop'><h6>Student Council</h6></a>
+          <Link to="/StudentCouncil">
+            <img src={studentCouncil}/><h6>Student Council</h6>
+            </Link>
           </div>
+          <Link to="/StudentCourt">
           <div className='student' id='court'>
             <img src={studentCouncil}/>
             <a href='#courtPop'><h6>Student Court</h6></a>
           </div>
+          </Link>
+          <Link to="/StudentCongress">
           <div className='student' id='congress'>
             <img src={studentCouncil}/>
             <a href='#congressPop'><h6>Student Congress</h6></a>
-          </div>          
+          </div>        
+          </Link>  
         </div>
         <div id='councilPop' className='popup'>
           <a href="#">&times;</a>
