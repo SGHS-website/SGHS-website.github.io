a089b4a86ee6aff20b12ecf3226b9dabd51b58e8
syslisa <syslisa0609@gmail.com>
2022-10-27 19:32:39 +0900

    club detail

--- src/pages/clubDetail.js
+++ src/pages/clubDetail.js
@@ -4,6 +4,8 @@ const clubDetail = () => {
     return (
         <body style ={{backgroundColor: "black"}}>
         
+            
+        
             <h1>예술경영 칼럼 (활동)</h1>
             <hr/>
             <h2>간단한 활동 소개 글</h2>
