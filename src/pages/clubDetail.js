f27f666359eaeb7d415ea47cba47fb772c821984
syslisa <syslisa0609@gmail.com>
2022-11-03 18:38:42 +0900

    club detail chnage 1

--- src/pages/clubDetail.js
+++ src/pages/clubDetail.js
@@ -2,13 +2,7 @@ import React from "react";
 import "../styles/clubDetail.css"
 const clubDetail = () => {
     return (
-        <body style ={{backgroundColor: "black"}}>
-        
-            
-            <h1>도담다담</h1>
-            <h1>예술경영 칼럼 (활동)</h1>
-            <hr/>
-            <h2>간단한 활동 소개 글</h2>
+        <body>     
             <div class="selector">~~~~~~~~~~~~~~~~~~~~~~~~~~~~</div>
             <hr id="line2"></hr>
             <div class="wrap">
