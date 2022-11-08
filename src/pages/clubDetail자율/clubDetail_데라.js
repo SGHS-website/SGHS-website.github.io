752bd5e14e3d7fb0d96c49091833b6cbe8145dba
lee56781 <221612@sghs.sen.hs.kr>
2022-11-08 19:21:40 +0900

    .

--- src/pages/clubDetail자율/clubDetail_데라.js
+++ src/pages/clubDetail자율/clubDetail_데라.js
@@ -1,7 +1,7 @@
 import React from "react";
-import "../styles/clubDetail.css"
-import schoolBackground from "../images/background.jpg";
-import Footer from './Footer'
+import ".../styles/clubDetail.css"
+import schoolBackground from ".../images/background.jpg";
+import Footer from '../Footer'
 const clubDetail_데라 = () => {
     return (
         <body style={{backgroundColor:"black"}}>
