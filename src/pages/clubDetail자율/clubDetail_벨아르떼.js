8006813fbaed8366244c7a41938ad9ab1226f751
lee56781 <221612@sghs.sen.hs.kr>
2022-11-08 19:10:45 +0900

    .

--- src/pages/clubDetail자율/clubDetail_벨아르떼.js
+++ src/pages/clubDetail자율/clubDetail_벨아르떼.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/clubDetail.css"
 import schoolBackground from "../images/background.jpg";
 import Footer from './Footer'
-const clubDetail_벨아르떼BRASSBOUND= () => {
+const clubDetail_벨아르떼= () => {
     return (
         <body style={{backgroundColor:"black"}}>
         <div className='banner'>
@@ -11,7 +11,7 @@ const clubDetail_벨아르떼BRASSBOUND= () => {
         </div>
         <img src="" class="club-detail-img"></img>
           <div class="club-explain">
-                <h1 class="club-detail-explain">벨아르떼BRASSBOUND</h1>
+                <h1 class="club-detail-explain">벨아르떼</h1>
                 <p> 금관과 플룻, 클라리넷 등의 다양한 악기들의 합주를 통해 즐거움을 주는 오케스트라 동아리
                 </p>
                 <span className="club-detail-member">부장-설국다움<br></br>차장-설국다움</span>
@@ -93,4 +93,4 @@ const clubDetail_벨아르떼BRASSBOUND= () => {
     );
   };
   
-  export default clubDetail_벨아르떼BRASSBOUND;
+  export default clubDetail_벨아르떼;
