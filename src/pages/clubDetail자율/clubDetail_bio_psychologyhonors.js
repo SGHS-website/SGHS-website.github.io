7dd1f4ae7f6f8511f290e98d8867bd3aa1557cb9
syslisa <syslisa0609@gmail.com>
2022-11-10 18:23:11 +0900

    club detail changes 7

--- src/pages/clubDetail자율/clubDetail_bio_psychologyhonors.js
+++ src/pages/clubDetail자율/clubDetail_bio_psychologyhonors.js
@@ -1,7 +1,7 @@
 import React from "react";
-import "../styles/clubDetail.css"
-import schoolBackground from "../images/background.jpg";
-import Footer from './Footer'
+import "../../styles/clubDetail.css"
+import schoolBackground from "../../images/background.jpg";
+import Footer from '../Footer'
 const clubDetail_bio_psychologyhonors = () => {
     return (
         <body style={{backgroundColor:"black"}}>
