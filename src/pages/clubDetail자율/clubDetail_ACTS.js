48dbad0891eea68baa6526ed8475577f7f801cfc
syslisa <syslisa0609@gmail.com>
2022-11-15 18:13:11 +0900

    ACTS changes

--- src/pages/clubDetail자율/clubDetail_ACTS.js
+++ src/pages/clubDetail자율/clubDetail_ACTS.js
@@ -2,14 +2,14 @@ import React from "react";
 import "../../styles/clubDetail.css"
 import schoolBackground from "../../images/background.jpg";
 import Footer from '../Footer'
-const clubDetail_ACTS = () => {
+const ACTS = () => {
     return (
         <body style={{backgroundColor:"black"}}>
         <div className='banner'>
           <img src={schoolBackground}></img>
           <h2>club</h2>
         </div>
-        <img src={require("../../images/img/액츠.jpg")} class="club-detail-img"></img>
+        <img src="" class="club-detail-img"></img>
           <div class="club-explain">
                 <h1 class="club-detail-explain">ACTS</h1>
                 <p>함께 예배를 드리고 셀모임을 통해 교제를 나누는 기독교 동아리입니다
@@ -18,65 +18,9 @@ const clubDetail_ACTS = () => {
                 <span className="club-detail-date">2021.01.01</span>
           </div>
           <p class="club-detail-activity-name">예술경영 칼럼 및 문화 생활 관람</p>
-        <div class="club-detail-activity">
-            <p>간단한 소개와 글~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            </p>
-            <div class="club-detail-line"></div>
-        </div>
-            <div class="club-wrap">
-            <div class="sticker1">
-                <img src=""></img>
-            </div>
-            <div class="sticker1">
-                <img src=""></img>
-            </div>
-            <div class="sticker1">
-                <img src=""></img>
-            </div>
-            <div class="sticker1">
-                <img src=""></img>
-            </div>
-            <div class="sticker1">
-                <img src=""></img>
-            </div>
-            <div class="sticker1">
-                <img src=""></img>
-            </div>
-            <div class="sticker1">
-                <img src=""></img>
-            </div>
-            <div class="sticker1">
-                <img src=""></img>
-            </div>
-            </div>
-        <p class="club-detail-activity-name">예술경영 칼럼 및 문화 생활 관람</p>
-        <div class="club-detail-activity">
-            <p>간단한 소개와 글~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            </p>
-            <div class="club-detail-line"></div>
-        </div>
-        <div class="club-wrap">
-            <div class="sticker1">
-                <img src=""></img>
-            </div>
-            <div class="sticker1">
-                <img src=""></img>
-            </div>
-            <div class="sticker1">
-                <img src=""></img>
-            </div>
-            <div class="sticker1">
-                <img src=""></img>
-            </div>
-            <div class="sticker1">
-                <img src=""></img>
-            </div>
+          <div class="club-wrap">
+                <p>예수님의 흔적을 남기는 사람들 - 예배<br></br>
+                                            <ul></ul></p>
             <div class="sticker1">
                 <img src=""></img>
             </div>
@@ -93,4 +37,4 @@ const clubDetail_ACTS = () => {
     );
   };
   
-  export default clubDetail_ACTS;
+  export default ACTS;
